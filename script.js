ef91ae8f5aa5a94fd8b4a45e2ba397f3c1232668
LucasJa1994 <52300483+LucasJa1994@users.noreply.github.com>
2023-12-22 18:16:27 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleMode(){
+const html = document.documentElement
+html.classList.toggle('light')
+const img =  document.querySelector('#perfil img')
+
+ if (html.classList.contains('light')){
+    img.setAttribute('src','../Projeto/assets/avatar-light.png')
+    img.setAttribute('alt','foto de uma pessoa que nao é o lucas com ctz')
+
+}else {
+    img.setAttribute('src','../Projeto/assets/avatar.png')
+    img.setAttribute('alt', 'foto de Lucas josue alves de terno sorrindo fundo creme')
+
+}
+
+    // pegar a tag img e substituir a imagem
+}
